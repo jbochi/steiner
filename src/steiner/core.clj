030639cc3a5d7bd9b8f7831ac2491dd521b5d95f
Juarez Bochi <jbochi@gmail.com>
2013-10-04 07:10:02 -0300

    More idiomatic clojure

--- src/steiner/core.clj
+++ src/steiner/core.clj
@@ -76,14 +76,14 @@
 (defn steiner-point [triangle]
   "steiner-point of a triangle"
   (let [[A B C] triangle
-        d60 (* 1/3 Math/PI)
-        cross-product (det (v- A B) (v- A C))
-        direction (if (> cross-product  0) d60 (- d60))
-        C* (rotate-on-point A B (- direction))
-        B* (rotate-on-point A C direction)
         max-angle (* 2/3 Math/PI)
         angs (angles triangle)
         degenerate (keep-indexed #(if (> %2 max-angle) %1) angs)]
-    (if (empty? degenerate)
-      (intersection B B* C C*)
-      (nth triangle (first degenerate)))))
+    (if (seq degenerate)
+      (nth triangle (first degenerate))
+      (let [d60 (* 1/3 Math/PI)
+            cross-product (det (v- A B) (v- A C))
+            direction (if (> cross-product  0) d60 (- d60))
+            C* (rotate-on-point A B (- direction))
+            B* (rotate-on-point A C direction)]
+        (intersection B B* C C*)))))
