201b0b21d3b5c5a8cef97ce3dea50db259acd6a2
Juarez Bochi <jbochi@gmail.com>
2013-10-04 07:35:42 -0300

    Deestruct only when needed

--- src/steiner/core.clj
+++ src/steiner/core.clj
@@ -3,13 +3,13 @@
 
 (defn steiner-point [triangle]
   "steiner-point of a triangle"
-  (let [[A B C] triangle
-        max-angle (* 2/3 Math/PI)
+  (let [max-angle (* 2/3 Math/PI)
         angs (angles triangle)
         degenerate-index (keep-indexed #(if (> %2 max-angle) %1) angs)]
     (if (seq degenerate-index)
       (nth triangle (first degenerate-index))
-      (let [d60 (* 1/3 Math/PI)
+      (let [[A B C] triangle
+            d60 (* 1/3 Math/PI)
             cross-product (det (v- A B) (v- A C))
             direction (if (> cross-product  0) d60 (- d60))
             C* (rotate-on-point A B (- direction))
